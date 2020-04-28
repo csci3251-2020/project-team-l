9f3198fe0027424872f1eddda2951ec4fe254e4b
FBKingLOK <62502174+FBKingLOK@users.noreply.github.com>
2020-04-28 14:40:56 +0800

    Update code.c
    For task 6

--- code.c
+++ code.c
@@ -1,1 +1,15 @@
-// Your C code here
+#include <stdio.h>
+
+main()
+{
+ printf("Hello World!\n");
+ 
+ int i = 0;
+ for(i = 0; i < 10; i++)
+   printf("%d ",i);
+ 
+ int x = 0;
+ int y = 1;
+ if (x==y) printf("\nCOCO"); 
+ else printf("\nCOCOA"); 
+}
